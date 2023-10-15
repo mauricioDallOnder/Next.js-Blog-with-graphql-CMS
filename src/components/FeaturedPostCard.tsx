6875ce41a0cf64fa134faa88d3f0ccd1518582bf
mauricioDallOnder <londer11@icloud.com>
2023-10-15 10:53:18 -0300

    fixed SEO and semantics

--- src/components/FeaturedPostCard.tsx
+++ src/components/FeaturedPostCard.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import moment from 'moment';
 import Image from 'next/image';
 import Link from 'next/link';
 import Slider from 'react-slick';
 import { IIPostsResponse } from '@/interfaces/interfaces';
 
 const FeaturedPostCarousel = ({ posts }:IIPostsResponse) => {
-    
   const settings = {
     dots: false,
     infinite: true,
@@ -19,30 +17,26 @@ const FeaturedPostCarousel = ({ posts }:IIPostsResponse) => {
   };
 
   return (
-    <div className='mb-2'>
-    <Slider {...settings}>
+    <section className='mb-2'>
+      <Slider {...settings}>
         {posts.map((post) => (
-          <div key={post.title} className="relative h-72">
+          <article key={post.title} className="relative h-72">
             <div className="absolute rounded-lg bg-center bg-no-repeat bg-cover shadow-md inline-block w-full h-72" style={{ backgroundImage: `url('${post.featuredImage.url}')` }} />
             <div className="absolute rounded-lg bg-center bg-gradient-to-b opacity-50 from-gray-400 via-gray-700 to-black w-full h-72" />
-            <div className="flex flex-col rounded-lg p-4 items-start justify-center absolute w-full h-full pl-5"> {/* Alterado 'items-center' para 'items-start' */}
-              <p className="text-white mb-4 text-shadow font-semibold text-3xl pl-5">{post.title}</p>
+            <div className="flex flex-col rounded-lg p-4 items-start justify-center absolute w-full h-full pl-5">
+              <h2 className="text-white mb-4 text-shadow font-semibold text-3xl pl-5">{post.title}</h2> 
               <p className="text-white mb-4 text-shadow font-semibold text-xl pl-5">{post.excerpt}</p>
-              <div className="flex items-center absolute bottom-5 w-full justify-start pl-5"> {/* Alterado 'justify-center' para 'justify-start' */}
-              <Link href={`/post/${post.slug}`}>
-              <button className='bg-[#6100C2] hover:bg-[#6100C2] inline-flex items-start gap-3 px-6 py-[17px] rounded-[14px] text-[#FFFFFF]'>Continuar lendo</button>
-              </Link>
-               
+              <div className="flex items-center absolute bottom-5 w-full justify-start pl-5">
+                <Link href={`/post/${post.slug}`}>
+                  <button className='bg-[#6100C2] hover:bg-[#6100C2] inline-flex items-start gap-3 px-6 py-[17px] rounded-[14px] text-[#FFFFFF]'>Continuar lendo</button>
+                </Link>
               </div>
             </div>
-           
-           
-          </div>
+          </article>
         ))}
       </Slider>
-      </div>
+    </section>
   );
 };
-//bg-[#6100C2]
 
 export default FeaturedPostCarousel;
