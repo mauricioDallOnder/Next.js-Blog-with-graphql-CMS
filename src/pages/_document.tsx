dabc480bbba7fef58c025dcabbb07e3e75737351
mauricioDallOnder <londer11@icloud.com>
2023-10-15 20:41:30 -0300

    add adsense script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="pt-br">
-      <Head />
+      <Head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7645741188042839"
+     crossOrigin="anonymous"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
