32dd387f3b9a607fa1e18e394889caba6ff62343
mauricioDallOnder <londer11@icloud.com>
2023-10-15 21:27:13 -0300

    fix cookies

--- src/components/CookieBanner.tsx
+++ src/components/CookieBanner.tsx
@@ -29,23 +29,30 @@ const CookieConsent: React.FC = () => {
     <div className="fixed bottom-0 left-0 right-0 bg-gray-900 text-white p-4 z-50">
       <p>Usamos cookies para melhorar sua experiência. Selecione os cookies que deseja permitir:</p>
       <label>
-        <input type="checkbox" checked disabled /> Necessary
+        <input type="checkbox" checked disabled /> NECESSÁRIOS
       </label>
       <label>
         <input 
           type="checkbox" 
           checked={consent.performance} 
           onChange={() => setConsent(prev => ({ ...prev, performance: !prev.performance }))} 
-        /> Performance
+        /> FUNCIONAIS
       </label>
       <label>
         <input 
           type="checkbox" 
           checked={consent.marketing} 
           onChange={() => setConsent(prev => ({ ...prev, marketing: !prev.marketing }))} 
-        /> Marketing
+        /> ESTATÍSTICOS
       </label>
-      <button onClick={handleConsent} className="mt-2 bg-blue-500 px-4 py-2 rounded">
+      <label>
+        <input 
+          type="checkbox" 
+          checked={consent.marketing} 
+          onChange={() => setConsent(prev => ({ ...prev, marketing: !prev.marketing }))} 
+        /> MARKETING
+      </label>
+      <button onClick={handleConsent} className="mt-2 bg-blue-500 px-4 ml-2 py-2 rounded">
         Salvar preferencias
       </button>
     </div>
