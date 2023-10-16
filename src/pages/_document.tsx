da8cc8833ce66366eec3cb28b27bb6a488f0e964
mauricioDallOnder <londer11@icloud.com>
2023-10-15 23:05:27 -0300

    add google verification

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
   return (
     <Html lang="pt-br">
       <Head>
+      <meta name="google-site-verification" content="fA_xQB0J5slMw14GbDBUJD3nfwy8PAl8V-8-aembKHg" />
       <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7645741188042839"
      crossOrigin="anonymous"/>
       </Head>
