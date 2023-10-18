fb041539f37ff62353f41bcec5fbc887e97bc2c1
mauricioDallOnder <londer11@icloud.com>
2023-10-18 20:21:26 -0300

    fix issues

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -14,7 +14,7 @@ const PostCard = ({ title, excerpt, featuredImage, slug, createdAt, author }: IP
           objectFit="cover"
           className="shadow-lg rounded-t-lg lg:rounded-lg"
           priority={false}
-          quality={60}
+          quality={70}
           sizes="(min-width: 1540px) 1456px, (min-width: 1300px) 1200px, (min-width: 1160px) 944px, (min-width: 800px) 688px, (min-width: 660px) 560px, (min-width: 440px) 360px, 220px"
         />
       </figure>
