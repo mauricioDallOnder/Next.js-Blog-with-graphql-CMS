3ff625452d49cad733eecd1269c7a839573a054e
mauricioDallOnder <londer11@icloud.com>
2023-10-18 19:57:25 -0300

    fix issues

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -50,7 +50,7 @@ const PostCard = ({ title, excerpt, featuredImage, slug, createdAt, author }: IP
       <div className="text-center">
         <Link href={`/post/${slug}`}>
           <button className="transition duration-500 ease transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer" aria-label={`Continue lendo o post "${title}"`} >
-            Continuar lendo
+            clique aqui para continuar a leitura
           </button>
         </Link>
       </div>
