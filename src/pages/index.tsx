0319116703f845fe32351fc7a0d086289339dbb2
mauricioDallOnder <londer11@icloud.com>
2023-10-19 20:44:07 -0300

    fix minior issues

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -82,8 +82,8 @@ export default function Home({
       ${currentPage === totalPages ? "opacity-50 cursor-not-allowed" : ""}`}
       aria-label="próxima pagina"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6  text-white w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 text-white w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
         </svg>
           </button>
         </section>
