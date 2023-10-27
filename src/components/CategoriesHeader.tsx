9ccc0062fb29da32e748c09fe064c30431132b4f
mauricioDallOnder <londer11@icloud.com>
2023-10-26 23:45:55 -0300

    fix issues

--- src/components/CategoriesHeader.tsx
+++ src/components/CategoriesHeader.tsx
@@ -1,5 +1,3 @@
-import { faFolderOpen } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 type CategoriesHeaderProps = {
@@ -12,9 +10,7 @@ export default function CategoriesHeader({ name }:CategoriesHeaderProps){
       <div className="bg-[#3399cc] h-[5px]"></div>
       <header className="py-8 px-8 border-b border-gray-200">
         <div className="flex items-center">
-          <div className="text-4xl text-gray-700 mr-4">
-            <FontAwesomeIcon icon={faFolderOpen} aria-hidden="true" /> {/* Adiciona aria-hidden para ícones para melhor acessibilidade */}
-          </div>
+          
          
           <h2 className="text-4xl leading-snug text-gray-800 font-semibold">Categoria: {name}</h2>
         </div>
