ce8814abe19fa15a9eb7f7a42c333d2168bd9449
Mauricio Dall Onder <londer11@icloud.com>
2023-10-27 14:24:47 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'false',
 });
@@ -8,6 +7,20 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['firebasestorage.googleapis.com', 'media.graphassets.com'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:all*(svg|jpg|png)',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, must-revalidate',
+          }
+        ],
+      },
+    ]
   },
 }
 
