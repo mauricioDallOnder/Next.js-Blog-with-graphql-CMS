8d6f73a82d61b0b8b88ce40570eff28102cab7f0
mauricioDallOnder <londer11@icloud.com>
2023-10-27 20:55:27 -0300

    fix header layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,13 @@ import Link from "next/link";
 import { getCategories } from "../services";
 import { useRouter } from "next/router";
 import Image from "next/image";
-import { CloseIcon,HamburgerIcon,TriangleDownIcon,Search2Icon } from '@chakra-ui/icons'
-export default function Header(){
-
+import {
+  CloseIcon,
+  HamburgerIcon,
+  TriangleDownIcon,
+  Search2Icon,
+} from "@chakra-ui/icons";
+export default function Header() {
   const [categories, setCategories] = useState<
     { name: string; slug: string }[]
   >([]);
@@ -64,41 +68,41 @@ export default function Header(){
 
   return (
     <header className="container mx-auto px-4 md:px-10 mb-8">
-    <nav
-      className="border-b w-full border-blue-400 py-3 bg-gradient-to-rb from-6d327c via-485DA6 to-32b37b"
-      aria-label="Main navigation"
-    >
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link href="/" passHref>
-          <div className="flex items-center cursor-pointer">
-            <Image
-              src={
-                "https://firebasestorage.googleapis.com/v0/b/geradorimagens-27342.appspot.com/o/blog-cha%2FScreenshot_2023-10-15_at_16.18.14-removebg-preview.png?alt=media&token=e8f69fdf-c6f1-4788-bd37-68603b35673e&_gl=1*1cwpgw7*_ga*MTA2NDY5MTI4MS4xNjk3MzkyMDc0*_ga_CW55HF8NVT*MTY5NzM5NzMzMS4yLjEuMTY5NzM5Nzg2Ni40NS4wLjA."
-              }
-              alt="Logotipo chacomsabor"
-              width={200}
-              height={241}
-            />
-          </div>
-        </Link>
+      <nav
+        className="border-b w-full border-blue-400 py-3 bg-gradient-to-rb from-6d327c via-485DA6 to-32b37b "
+        aria-label="Main navigation"
+      >
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <Link href="/" passHref>
+            <div className="flex items-center cursor-pointer">
+              <Image
+                src={
+                  "https://firebasestorage.googleapis.com/v0/b/geradorimagens-27342.appspot.com/o/blog-cha%2FScreenshot_2023-10-15_at_16.18.14-removebg-preview.png?alt=media&token=e8f69fdf-c6f1-4788-bd37-68603b35673e&_gl=1*1cwpgw7*_ga*MTA2NDY5MTI4MS4xNjk3MzkyMDc0*_ga_CW55HF8NVT*MTY5NzM5NzMzMS4yLjEuMTY5NzM5Nzg2Ni40NS4wLjA."
+                }
+                alt="Logotipo chacomsabor"
+                width={200} 
+                height={70} 
+              />
+            </div>
+          </Link>
 
-        <button
-          onClick={() => setMenuOpen(!menuOpen)}
-          type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden focus:outline-none "
-          aria-label="Open main menu"
-        >
-          {menuOpen ? <HamburgerIcon /> : <CloseIcon />}
-        </button>
+          <button
+            onClick={() => setMenuOpen(!menuOpen)}
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden focus:outline-none "
+            aria-label="Open main menu"
+          >
+            {menuOpen ? <HamburgerIcon style={{ fontSize: '24px', color: '#FFFFFF' }} /> : <CloseIcon style={{ fontSize: '24px', color: '#fa0000' }} />}
+          </button>
 
-        <div
-          className={`${
-            menuOpen ? "block" : "hidden"
-          } w-full md:block md:w-auto md:pt-0 md:mt-0`}
-          id="navbar-dropdown"
-          aria-label="Menu de navegação principal"
-        >
-           <ul className="flex flex-col items-center font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 ">
+          <div
+            className={`${
+              menuOpen ? "block" : "hidden"
+            } w-full md:block md:w-auto md:pt-0 md:mt-0`}
+            id="navbar-dropdown"
+            aria-label="Menu de navegação principal"
+          >
+            <ul className="flex flex-col items-center font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 ">
               <li>
                 <Link href="/" passHref>
                   <span className="block py-2 pl-3 pr-4 text-white font-bold bg-transparent md:hover:text-red  hover:underline md:p-0">
@@ -153,52 +157,49 @@ export default function Header(){
                 </Link>
               </li>
               <li className="flex items-center">
-              <div className="flex flex-col md:flex-row justify-start gap-2 items-center">
-                {isInputVisible ? (
-                  <div className="flex items-center align-middle gap-2">
-                    <input
-                      className="pl-2 bg-white border border-gray-300 w-full md:w-88 text-lg font-medium text-black focus:border-yellow-500 focus:outline-none rounded-md py-2"
-                      placeholder="pesquisar..."
-                      value={inputValue}
-                      onChange={handleChange}
-                      aria-label="Search"
-                    />
-                    <div className="flex mt-2 gap-2">
-                      <button
-                        onClick={handleSearch}
-                        className="custom-btn-blue bg-blue-600 text-white rounded-md px-4 py-2 cursor-pointer"
-                      >
-                        <Search2Icon />
-                        <span className="ml-2">Buscar</span>
-                      </button>
-                      <button
-                        onClick={toggleInputVisibility}
-                        className="custom-btn-red bg-red-600 text-white rounded-md px-4 py-2 cursor-pointer"
-                      >
-                        <CloseIcon />
-                      </button>
+                <div className="flex flex-col md:flex-row justify-start gap-2 items-center">
+                  {isInputVisible ? (
+                    <div className="flex items-center align-middle gap-2">
+                      <input
+                        className="pl-2 bg-white border border-gray-300 w-full md:w-88 text-lg font-medium text-black focus:border-yellow-500 focus:outline-none rounded-md py-2"
+                        placeholder="pesquisar..."
+                        value={inputValue}
+                        onChange={handleChange}
+                        aria-label="Search"
+                      />
+                      <div className="flex mt-2 gap-2">
+                        <button
+                          onClick={handleSearch}
+                          className="custom-btn-blue bg-blue-600 text-white rounded-md px-4 py-2 cursor-pointer"
+                        >
+                          <Search2Icon />
+                          <span className="ml-2">Buscar</span>
+                        </button>
+                        <button
+                          onClick={toggleInputVisibility}
+                          className="custom-btn-red bg-red-600 text-white rounded-md px-4 py-2 cursor-pointer"
+                        >
+                          <CloseIcon />
+                        </button>
+                      </div>
                     </div>
-                  </div>
-                ) : (
-                  <button
-                    onClick={toggleInputVisibility}
-                    className="custom-btn-blue bg-blue-600 text-white rounded-md px-4 py-2 cursor-pointer"
-                  >
-                    <Search2Icon />
-                    <span className="ml-2 font-semibold">Pesquisar</span>
-                  </button>
-                )}
-              </div>
-            </li>
-          </ul>
+                  ) : (
+                    <button
+                      onClick={toggleInputVisibility}
+                      className="custom-btn-blue bg-blue-600 text-white rounded-md px-4 py-2 cursor-pointer"
+                    >
+                      <Search2Icon />
+                      <span className="ml-2 font-semibold">Pesquisar</span>
+                    </button>
+                  )}
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-  </header>
+      </nav>
+    </header>
   );
-  
-
-};
-
+}
 
 //fix issues
