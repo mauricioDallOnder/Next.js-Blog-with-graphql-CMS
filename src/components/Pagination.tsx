fcfebb91ac7f70062af8c85f7f63f67ef258cdfa
mauricioDallOnder <londer11@icloud.com>
2023-11-11 23:18:48 -0300

    remove aria-hidden

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -36,7 +36,7 @@ const Pagination: React.FC<PaginationProps> = ({
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
             fill="currentColor"
-            aria-hidden="true"
+            
             className="w-5 h-5"
           >
             <path
@@ -69,7 +69,7 @@ const Pagination: React.FC<PaginationProps> = ({
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
             fill="currentColor"
-            aria-hidden="true"
+            
             className="w-5 h-5"
           >
             <path
